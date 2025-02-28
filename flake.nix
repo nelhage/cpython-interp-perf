3052edecbe94371467e46aaa6a4a2cc742a32470
Nelson Elhage <nelhage@nelhage.com>
2025-02-28 00:14:14 +0000

    Initial shim

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Playing with CPython interpreters";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.uv
+            ];
+
+            shellHook = ''
+              export UV_PYTHON=${pkgs.python313}/bin/python3
+            '';
+          };
+        }
+      );
+    };
+}
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from cpython-interp-perf!")
+
+
+if __name__ == "__main__":
+    main()
