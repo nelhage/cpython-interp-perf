efa7a97157b8d6631c0b920090cba5b397793d99
Nelson Elhage <nelhage@nelhage.com>
2025-03-06 01:28:25 +0000

    Fix the LLVM build
    - Override `libllvm`, which is the one actually used internally
    - Create our own patched stdenv

--- llvm.nix
+++ llvm.nix
@@ -1,6 +1,8 @@
 {
   llvmPackages_20,
   llvmPackages_19,
+  overrideCC,
+  stdenv,
   fetchFromGitHub,
   lib,
   ...
@@ -11,13 +13,15 @@ in
 {
   gh114990 = llvmPackages_19.tools.extend (
     final: prev: {
-      llvm = prev.llvm.overrideAttrs {
-        patches = prev.llvm.patches ++ [
+      libllvm = prev.libllvm.overrideAttrs {
+        patches = prev.libllvm.patches ++ [
           ./patches/0001-TailDup-Allow-large-number-of-predecessors-successor.patch
           ./patches/0002-Add-a-computed-goto-test.patch
           ./patches/0003-TailDuplicator-Do-not-restrict-the-computed-gotos.patch
         ];
       };
+
+      stdenv = overrideCC stdenv final.clang;
     }
   );
 }
