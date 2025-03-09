92d53c9781196aeb99ca794b9df5fe6294519cd4
Nelson Elhage <nelhage@nelhage.com>
2025-03-09 19:44:35 +0000

    Rename pybuilds -> python

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
         in
         rec {
           llvm = pkgs.callPackage ./llvm.nix { };
-          pybuilds = pkgs.callPackage ./python.nix {
+          python = pkgs.callPackage ./python.nix {
             llvm_gh114990 = llvm.gh114990;
             useCompatConfig = !pkgs.stdenv.isDarwin;
           };
